82d86ef84d9d7c67a263d5dd742bfd43461b6dbc
MIDOWWEBCRETOR <midowwebcreater@gmail.com>
2025-12-19 08:22:22 +0530

    Delete supabase-client.js

--- supabase-client.js
+++
@@ -1,7 +0,0 @@
-// supabase-client.js (module)
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-export const SUPA_URL = 'https://uwpbzsqqsjdbglwpmfsu.supabase.co'
-export const SUPA_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3cGJ6c3Fxc2pkYmdsd3BtZnN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4ODIyNDAsImV4cCI6MjA3OTQ1ODI0MH0.oMFQb9CTt63ipOJCtAlzyZ4qZiRLwIqiciobGZlmnHc'
-
-export const supabase = createClient(SUPA_URL, SUPA_ANON)
